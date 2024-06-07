b4f3525ab7584291fce66970bff18c0490af5c93
karthikeyiniD <128837288+karthikeyiniD@users.noreply.github.com>
2024-06-07 22:10:50 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
             {({ signOut, user }) => (
     <main>
                 <h1>{user?.signInDetails?.loginId}'s todos</h1>
-      <h1>My todos</h1>
+      
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
